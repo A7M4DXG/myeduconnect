fbbd933a5c9f51154d57183865612b5fbae22e33
A7M4DXG <ahmed.siddig.adam@student.mmu.edu.my>
2026-06-01 22:38:23 +0800

    Connect backend to database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const db = require('./config/db');
+require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+db.connect((err)=>{
+
+if (err) throw err;
+
+console.log("Database Connected");
+
+});
 
 app.get('/', (req,res)=>{
 
